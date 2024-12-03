3824bbd7cf1af6ddbaffcbd3832c832285bcc326
Yanzun Jiang <stary54264@gmail.com>
2024-12-03 00:29:30 -0500

    Fixed Code Style

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -19,12 +19,14 @@ package <- show_package("f5aa9b07-da35-45e6-b31f-d6790eb9bd9b")
 # get all resources for this package
 resources <- list_package_resources("f5aa9b07-da35-45e6-b31f-d6790eb9bd9b")
 
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+# identify datastore resources; by default, Toronto Open Data sets datastore
+# resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <-
+  filter(resources, tolower(format) %in% c("csv", "geojson"))
 
 # load the first datastore resource as a sample
-raw_data <- filter(datastore_resources, row_number()==1) |> get_resource()
+raw_data <- filter(datastore_resources, row_number() == 1) |> get_resource()
 
 
 #### Save data ####
-write_csv(raw_data, "data/01-raw_data/raw_data.csv") 
+write_csv(raw_data, "data/01-raw_data/raw_data.csv")
--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Setup a linear model to describe the relationship between facilities 
+# Purpose: Setup a linear model to describe the relationship between facilities
 #   and area of Toronto libraries.
 # Author: Yanzun Jiang
 # Date: 25 November 2024
@@ -19,13 +19,15 @@ data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
 
 ### Model data ####
-model <- stan_glm(
-    formula = dih ~ area + parking + workstations + year,
-    data = data,
-    family = binomial(link = "logit"),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    seed = 1125)
+model <-
+  stan_glm(formula = dih ~ area + parking + workstations + year,
+           data = data,
+           family = binomial(link = "logit"),
+           prior =
+           normal(location = 0, scale = 2.5, autoscale = TRUE),
+           prior_intercept =
+           normal(location = 0, scale = 2.5, autoscale = TRUE),
+           seed = 1125)
 
 
 #### Save model ####
